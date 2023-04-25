ae2c0650bddb7d41705cc110a37847fa36630d2e
vaibhav <vaibhavguy40@gmail.com>
2023-04-25 16:23:08 -0700

    Initial Commit

---
+++ Lock/LockApp.swift
@@ -0,0 +1,17 @@
+//
+//  LockApp.swift
+//  Lock
+//
+//  Created by Vaibhav Satishkumar on 4/25/23.
+//
+
+import SwiftUI
+
+@main
+struct LockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
